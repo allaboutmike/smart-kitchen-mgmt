1f7814c69113981bae236b1df2270f66c5922b38
treblig-punisher <gilbertplayx@hotmail.com>
2025-02-26 00:34:23 -0500

    CRM-18 Tested and build the project. Everything should build and work just fine. I removed the unused import that caused the issue. I also reworked the navbar component so we don't have to deal with unnecessary boiler plate.

--- frontend/src/components/SideNavBar.tsx
+++ frontend/src/components/SideNavBar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
-export default function SideNavBar({children}: {children: React.ReactNode}) {
+import svgIcons from "../app/svgIcons";
+import SideNavBarButton from "@/components/SideNavBarButton";
+export default function SideNavBar() {
   return (
     <div className="side-nav-bar">
-      {children}
+      <SideNavBarButton svgIcon={svgIcons.home} text="Home" />
+        <SideNavBarButton svgIcon={svgIcons.menuManager} text="Menu Manager" />
+        <SideNavBarButton svgIcon={svgIcons.orderTracking} text="Order Tracking" />
+        <SideNavBarButton svgIcon={svgIcons.inventory} text="Inventory Tracker" />
+        <SideNavBarButton svgIcon={svgIcons.productivity} text="Productivity Metrics" />
+        <SideNavBarButton svgIcon={svgIcons.settings} text="Settings" />
     </div>
   )
 }
