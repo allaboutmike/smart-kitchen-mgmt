83680f03abb410e2f23f365dc41d1e559063fd2b
treblig-punisher <gilbertplayx@hotmail.com>
2025-02-26 15:22:17 -0500

    Implemented the option selection functionality the sidebar was missing. The button component has been modified to make all of this work.

--- frontend/src/components/SideNavBar.tsx
+++ frontend/src/components/SideNavBar.tsx
@@ -1,15 +1,19 @@
+"use client";
+import { useState } from 'react';
 import React from 'react'
 import svgIcons from "../app/svgIcons";
 import SideNavBarButton from "@/components/SideNavBarButton";
 export default function SideNavBar() {
+  const [currentSelection, setCurrentSelection] = useState("Home");
+  const updatedText = currentSelection;
   return (
     <div className="side-nav-bar">      
-        <SideNavBarButton svgIcon={svgIcons.home} text="Home" />
-        <SideNavBarButton svgIcon={svgIcons.menuManager} text="Menu Manager" />
-        <SideNavBarButton svgIcon={svgIcons.orderTracking} text="Order Tracking" />
-        <SideNavBarButton svgIcon={svgIcons.inventory} text="Inventory Tracker" />
-        <SideNavBarButton svgIcon={svgIcons.productivity} text="Productivity Metrics" />
-        <SideNavBarButton svgIcon={svgIcons.settings} text="Settings" />      
+        <SideNavBarButton svgIcon={svgIcons.home} currentText={updatedText} setCurrentSelection={setCurrentSelection} text="Home" />
+        <SideNavBarButton svgIcon={svgIcons.menuManager} currentText={updatedText} setCurrentSelection={setCurrentSelection} text="Menu Manager" />
+        <SideNavBarButton svgIcon={svgIcons.orderTracking} currentText={updatedText} setCurrentSelection={setCurrentSelection} text="Order Tracking" />
+        <SideNavBarButton svgIcon={svgIcons.inventory} currentText={updatedText} setCurrentSelection={setCurrentSelection} text="Inventory Tracker" />
+        <SideNavBarButton svgIcon={svgIcons.productivity} currentText={updatedText} setCurrentSelection={setCurrentSelection} text="Productivity Metrics" />
+        <SideNavBarButton svgIcon={svgIcons.settings} currentText={updatedText} setCurrentSelection={setCurrentSelection} text="Settings" />      
     </div>
   )
 }
--- frontend/src/components/SideNavBarButton.tsx
+++ frontend/src/components/SideNavBarButton.tsx
@@ -1,12 +1,30 @@
+"use client";
 import React from 'react'
+import {useEffect, useRef} from 'react';
 
 interface SideNavBarButtonProps {
     svgIcon: React.ReactNode;
     text: string;
+    setCurrentSelection: (selection: string) => void;
+    currentText: string;
 }
-export default function SideNavBarButton({svgIcon, text}: SideNavBarButtonProps) {
+export default function SideNavBarButton({svgIcon, text, setCurrentSelection, currentText}: SideNavBarButtonProps) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  useEffect(() => {
+    if(!buttonRef.current) return;
+    if(currentText == text){
+      buttonRef.current.style.backgroundColor = "var(--foreground)";      
+      buttonRef.current.style.color = "var(--background)";   
+    }else{
+      buttonRef.current.style.backgroundColor = "initial";
+      buttonRef.current.style.color = "var(--foreground)";
+    }
+
+  }, [currentText, text]);
   return (
-    <button className="side-nav-bar-button">
+    <button className="side-nav-bar-button" ref={buttonRef} onPointerDown={() => {
+      setCurrentSelection(text);
+    }}>
       {svgIcon}
       <div>{text}</div>
     </button>
