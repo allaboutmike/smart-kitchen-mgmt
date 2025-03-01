4c0818d9151d8924ee9d80a6dc4879356e568142
treblig-punisher <gilbertplayx@hotmail.com>
2025-03-01 00:20:18 -0500

    CRM-26 Deleted a file that was no longer needed.

--- frontend/src/components/RestaurantFoodMenuContainer.tsx
+++
@@ -1,7 +0,0 @@
-// interface RestaurantFoodMenuContainerProps{
-//     choice: string;
-// }
-// export default function RestaurantFoodMenuContainer({choice}: RestaurantFoodMenuContainerProps){
-
-
-// }
