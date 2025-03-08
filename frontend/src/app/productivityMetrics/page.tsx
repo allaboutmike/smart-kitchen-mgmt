3c46305b601e237fb280f22577bf5f039b290151
treblig-punisher <gilbertplayx@hotmail.com>
2025-03-08 03:26:44 -0500

    CRM-20 Removed all toast message components from the Productivity page.

--- frontend/src/app/productivityMetrics/page.tsx
+++ frontend/src/app/productivityMetrics/page.tsx
@@ -1,27 +1,10 @@
 import GridWasteMetrics from '@/components/GridWasteMetrics'
-import ToastMessageNotification from '@/components/ToastMessageNotification'
 import React from 'react'
 
 export default function ProductivityMetricsPage() {
   return (
     <div className="main-container">
-      <GridWasteMetrics />
-      <ToastMessageNotification 
-        alertMessage='An impressive alert message'
-        alertName={"alert alert-info"}
-      />
-      <ToastMessageNotification 
-        alertMessage='An impressive alert message'
-        alertName={"alert alert-error"}
-      />
-      <ToastMessageNotification 
-        alertMessage='An impressive alert message'
-        alertName={"alert alert-success"}
-      />
-      <ToastMessageNotification 
-        alertMessage='An impressive alert message'
-        alertName={"alert alert-warning"}
-      />
+      <GridWasteMetrics />      
     </div>
   )
 }
