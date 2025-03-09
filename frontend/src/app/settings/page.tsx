0d2c790c5e503bd3c55e116dd09f480f6de9ab14
treblig-punisher <gilbertplayx@hotmail.com>
2025-03-08 19:57:19 -0500

    CRM-20 reworked what string the user is required to pass into the toast message component.

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,7 +1,14 @@
+import ToastMessageNotification from '@/components/ToastMessageNotification'
 import React from 'react'
 
 export default function SettingsPage() {
   return (
-    <div className="main-container">Settings</div>
+    <>
+      <div className="main-container">Settings</div>
+      <ToastMessageNotification alertName= "info"  alertMessage='some message'/>
+      <ToastMessageNotification alertName= {"warning"}  alertMessage='some message'/>
+      <ToastMessageNotification alertName= {"success"}  alertMessage='some message'/>
+      <ToastMessageNotification alertName= {"error"}  alertMessage='some message'/>
+    </>
   )
 }
