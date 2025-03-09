34374bd8a8ea543ce12b331f1b5f04a3f6d49710
treblig-punisher <gilbertplayx@hotmail.com>
2025-03-09 01:26:16 -0500

    CRM-20 Extracted the SideNavBar into its own css module.

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,14 +1,9 @@
-import ToastMessageNotification from '@/components/ToastMessageNotification'
 import React from 'react'
 
 export default function SettingsPage() {
   return (
     <>
       <div className="main-container">Settings</div>
-      <ToastMessageNotification alertName= "info"  alertMessage='some message'/>
-      <ToastMessageNotification alertName= {"warning"}  alertMessage='some message'/>
-      <ToastMessageNotification alertName= {"success"}  alertMessage='some message'/>
-      <ToastMessageNotification alertName= {"error"}  alertMessage='some message'/>
     </>
   )
 }
