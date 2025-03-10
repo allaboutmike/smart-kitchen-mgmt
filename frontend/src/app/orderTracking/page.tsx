4f89663c1668782022120d29359c1e825edea32d
treblig-punisher <gilbertplayx@hotmail.com>
2025-03-09 22:47:16 -0400

    CRM-81 I've extracted even more css into their own modules. I've created the OrderDetailsScreen component for the Order tracking page.

--- frontend/src/app/orderTracking/page.tsx
+++ frontend/src/app/orderTracking/page.tsx
@@ -6,25 +6,23 @@ import OrderReceiptManager, { Order } from "@/components/OrderReceiptManager";
 
 export default function OrderTrackingPage() {
   const { data: orders } = useFetch<Order[]>("/orders/1");
+  console.table(orders)
 
   return (
     <div className="main-container">
       <OrderTrackingMenu />
-      <div>
-        {orders && orders.length ? (
-          <div>
-            <h1>Orders</h1>
-            {orders.map((order) => (
-              <div key={order.id}>
-                <OrderReceiptManager order={order} />
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            <h1>No Orders</h1>
-          </div>
-        )}
+      <div>        
+          <h1>{orders? "Orders": "No Orders"}</h1>
+          <div className={"scrollable-orders-container"}>
+            {(orders && orders.length > 0) &&
+              orders.map((order)=>{
+                return <OrderReceiptManager 
+                key={order.id} 
+                order={order} 
+              />
+              })
+            }
+          </div>        
       </div>
     </div>
   );
