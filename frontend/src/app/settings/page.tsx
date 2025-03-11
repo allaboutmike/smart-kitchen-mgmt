1e6101cd0506a8973741f60c1bae0a374a2d9c03
Gilbert Matos <gilbertplayx@hotmail.com>
2025-03-11 11:39:47 -0400

    Update page.tsx
    removed the unnecessary <></>. Forgot to remove it after I was done testing.

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,7 +1,5 @@
 export default function SettingsPage() {
   return (
-    <>
-      <div className="main-container">Settings Page :D</div>
-    </>
+    <div className="main-container">Settings</div>
   );
 }
