f53afd90a01cc19c80d23d4f0ef45eb6d55211e6
kevinmarindev <kevin.marin7@hotmail.com>
2025-03-11 20:00:55 -0500

    minor bug fix

--- frontend/src/app/orderTracking/page.tsx
+++ frontend/src/app/orderTracking/page.tsx
@@ -5,24 +5,30 @@ import { useFetch } from "@/customHooks/useFetch";
 import OrderReceiptManager, { Order } from "@/components/OrderReceiptManager";
 
 export default function OrderTrackingPage() {
-  const { data: orders } = useFetch<Order[]>("/orders?");
-  console.table(orders)
+  // the orders coming from the backend look something like this {
+  //   "orderid": 54,
+  //     "ordertimestamp": "2025-01-15T13:09:19.000Z",
+  //       "completed": false,
+  //         "completedTimeStamp": null
+  // }, types do not match the only properties the orders have are the ones above 
+  const { data } = useFetch<{ orders: Order[] }>("/orders?completed=false");
+  console.log(data)
 
   return (
     <div className="main-container">
       <OrderTrackingMenu />
-      <div className="order-tracking-main-container">        
-          <h1>{orders? "Orders": "No Orders"}</h1>
-          <div className={"scrollable-orders-container"}>
-            {(orders && orders.length > 0) &&
-              orders.map((order)=>{
-                return <OrderReceiptManager 
-                key={order.id} 
-                order={order} 
+      <div className="order-tracking-main-container">
+        <h1>{data?.orders ? "Orders" : "No Orders"}</h1>
+        <div className={"scrollable-orders-container"}>
+          {(data && data.orders.length) &&
+            data.orders.map((order) => (
+              <OrderReceiptManager
+                key={order.id}
+                order={order}
               />
-              })
-            }
-          </div>        
+            ))
+          }
+        </div>
       </div>
     </div>
   );
