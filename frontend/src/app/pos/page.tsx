53f505ba92fb49129610d4ca94cd506c2363b41f
treblig-punisher <gilbertplayx@hotmail.com>
2025-03-19 20:12:19 -0400

    Implemented item stacking and item actions as well as cancelling the current order.

--- frontend/src/app/pos/page.tsx
+++ frontend/src/app/pos/page.tsx
@@ -1,23 +1,73 @@
 "use client"
-import Cart, { CartInfo, POSMenuItem, ItemProps } from '@/components/POS/Cart'
+import Cart, { CartInfo, ItemProps } from '@/components/POS/Cart'
 import { useFetch } from '@/customHooks/useFetch'
 import { useState } from 'react'
-import { createContext } from 'react'
-import { CartContextType } from '@/components/POS/Cart'
+import useSelection from '@/customHooks/useSelection'
+import { MenuItemType } from '@/components/MenuManagementContainer'
+import SelectableButton from '@/components/SelectableButton'
+import styles from "../../styles/MenuManagementContainer.module.css"
+import Image from 'next/image'
 
+const menuCategories =
+  "Popular, Meals, Entrees, Salads, Sides, Beverages".split(
+    ","
+  );
+menuCategories.forEach((menuCategory) => menuCategory.trimStart());
 export default function POS() {
     
-    const {data} = useFetch<{posMenuItems: POSMenuItem[]}>("menuItems")
+    const fetchString = "menuItems";
+    const { data } = useFetch<{ menuItems: MenuItemType[] }>(fetchString);
     const [addedItems, setAddedItems] = useState<ItemProps[]>([])
     const [isCartVisible, setIsCartVisible] = useState(false)
-    const menuItems = data?.posMenuItems
-    const cartInfo: CartInfo={
-      orderid: 939820,
-      items: addedItems 
+    const {currentSelection, isCurrentSelection, setCurrentSelection} = useSelection();
+    const menuItems = data?.menuItems
+    const cancelOrder =()=> setAddedItems([])
+    const increaseItemQty =(itemName: string)=>{
+        const items = [...addedItems]
+        const itemIndex = items.findIndex(item=> item.foodName === itemName)
+        if(items[itemIndex] && items[itemIndex].quantity){
+          items[itemIndex].quantity +=1;
+          setAddedItems([...items])
+        }
+    }
+    const decreaseItemQty =(itemName: string)=>{
+      const items = [...addedItems]
+      const itemIndex = items.findIndex(item=> item.foodName === itemName)
+      if(items[itemIndex] && items[itemIndex].quantity && items[itemIndex].quantity > 1) {
+        items[itemIndex].quantity -=1;
+        setAddedItems([...items])
+      }
+  }
+  const continueOrder =()=>{}
+  const removeItemFromCart =(itemName: string)=>{
+    const filteredItems = addedItems.filter(item=> item.foodName !== itemName)
+    setAddedItems([...filteredItems])
   }
   
-  const cancelOrder =()=> setAddedItems([])
-  const cartContext = createContext<CartContextType | null>(null);
+  const cartInfo: CartInfo={
+    orderid: 939820,
+    items: addedItems,
+    itemActions: {
+      increaseQty: increaseItemQty,
+      decreaseQty: decreaseItemQty,
+      removeItem: removeItemFromCart
+    },
+    continueOrder: continueOrder,
+    cancelOrder: cancelOrder
+  }
+
+  const filteredMenuItems = 
+  currentSelection === "none"
+  ? menuItems
+  : menuItems?.filter(item => {
+      const optionName = currentSelection.toLowerCase().trim();
+      if (optionName === "popular") {
+        return item.isPopular === true;
+      }
+      return item.category.toLowerCase() === optionName;
+    });
+  
+  
   const svgSize = 20
     console.table(menuItems)
   return (
@@ -25,27 +75,55 @@ export default function POS() {
         <button className="btn btn-square text-[--foreground] self-end mr-[20px] bg-[--background] hover:bg-[--foreground] hover:text-white border-[--foreground] mt-[20px]" onClick={()=> setIsCartVisible(prevVisibility=> !prevVisibility)}>
         <svg width={svgSize} height={svgSize} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 .75H1a1 1 0 0 0-1 1v.5a1 1 0 0 0 1 1h2.012l2.724 11.481A4.25 4.25 0 0 0 9.765 18h7.822a4 4 0 0 0 3.943-3.325l1.256-7.338A2 2 0 0 0 20.814 5H5.997l-.78-3.289A1.25 1.25 0 0 0 4 .75M10 21a2 2 0 1 1-4 0 2 2 0 0 1 4 0m11 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0" fill="currentColor"/></svg>
         </button>
+        <div className={styles["restaurant-sub-menu-container"]}>
+        {menuCategories.map((menuItem, index) => {
+          return (
+            <SelectableButton
+              selected={isCurrentSelection(menuItem)}
+              setCurrentSelection={setCurrentSelection}
+              buttonClassName={styles["restaurant-sub-menu-button"]}
+              text={menuItem.trimStart()}
+              key={index}
+            />
+          );
+        })}
+      </div>
+
         <span className="flex outline gap-[2rem] w-full justify-between place-items-center ml-[-20px] p-[20px] mt-[20px]">
           <span className="grid grid-flow-row-dense tablet:grid-cols-2 mobile:grid-cols-1 
           p-[20px] grid-cols-6 gap-[20px] max-h-[400px] overflow-y-auto" >
            {
-            Array.from({length: 200}).fill("Item").map((item, index)=>{
+            filteredMenuItems?.map((item, index)=>{
+              const imgSize = 100;
               return(
                 <span className="flex flex-col p-[10px] bg-white outline rounded-md place-content-center" key={index}>
-                  <span className="tablet:text-[0.7rem] text-center">Food Name</span>
+                  <Image src={item.pictureUrl} width={imgSize} height={imgSize} alt={item.name}/>
+                  <span className="tablet:text-[0.7rem] text-center">{item.name}</span>
+                  <span className="flex flex-col">
+                    <span>Price:</span>
+                    <span>${item.price}</span>
+                  </span>
                   <button className="btn tablet:text-[0.7rem]" onClick={()=>{
-                    const newItem: ItemProps = {foodName: "Name", foodPrice: 8.88, quantity: 1};
-                    setAddedItems(prevItems=> [...prevItems, newItem])
+                    const newItem: ItemProps = {foodName: item.name, foodPrice: parseFloat(item.price), quantity: 1};
+                    const allItems = [...addedItems]
+                    const itemFoundIndex = allItems.findIndex(item => item.foodName === newItem.foodName)
+                    if(itemFoundIndex >= 0 && itemFoundIndex < allItems.length && allItems[itemFoundIndex]?.quantity !== undefined) {
+                      allItems[itemFoundIndex].quantity += 1
+                      setAddedItems([...allItems])
+                    }
+                    else{
+                      setAddedItems(prevItems=> [...prevItems, newItem])
+                    }
+                    
                   }}>Add Item</button>
                 </span>
               )
             })
             }
           </span>
-          <cartContext.Provider value={{cancelOrder}}>
             { isCartVisible && <Cart {...cartInfo}/>}
-          </cartContext.Provider>
         </span>
     </div>
   )
 }
+
--- frontend/src/components/POS/Cart.tsx
+++ frontend/src/components/POS/Cart.tsx
@@ -6,29 +6,38 @@ export interface MenuItem {
   menuitemid: number;
   customizationdetail: string | null
 }
+export interface ItemButtonActions{
+  decreaseQty: (itemName: string)=>void;
+  increaseQty: (itemName: string)=>void;
+  removeItem:  (itemName: string)=>void;
+}
 export interface CartInfo{
     orderid: number;
     items: ItemProps[];
+    continueOrder: ()=>void;
+    cancelOrder: ()=>void;
+    itemActions: ItemButtonActions;
 }
 export interface POSMenuItem{    
-category: string;
-createdat: string;
-isPopular : boolean;
-menuitemid: number;
-name: string;
-pictureUrl: string;
-price: string
-profit: string;
-updatedat : string;
+  category: string;
+  createdat: string;
+  isPopular : boolean;
+  menuitemid: number;
+  name: string;
+  pictureUrl: string;
+  price: string;
+  profit: string;
+  updatedat : string;
 }
 
 export interface ItemProps{
   foodName: string;
   foodPrice: number;
   quantity?: number;
+  actions?: ItemButtonActions;
 }
 export default function Cart(orderInfo: CartInfo) {
-  const cartTotalCost = orderInfo.items.reduce((prevVal, currVal)=> prevVal + currVal.foodPrice, 0).toFixed(2)
+  const cartTotalCost = orderInfo.items.reduce((prevVal, currVal)=> prevVal + (currVal.quantity? (currVal.foodPrice * currVal.quantity): currVal.foodPrice), 0).toFixed(2)
   return (
     <div className="flex flex-col px-[40px] gap-[1rem] py-[20px] rounded-[8px] max-w-[500px] min-w-[300px] bg-white h-[500px] outline">
       <span className="max-h-[fit-content] text-2xl">Order:#{orderInfo.orderid}</span>
@@ -38,7 +47,8 @@ export default function Cart(orderInfo: CartInfo) {
                 const currItem: ItemProps={
                     foodName: item.foodName,
                     foodPrice: item.foodPrice,
-                    quantity: item.quantity
+                    quantity: item.quantity,
+                    actions: orderInfo.itemActions
                 }
                 return(
                     <ItemComponent key={index} {...currItem} />
@@ -53,7 +63,7 @@ export default function Cart(orderInfo: CartInfo) {
         </span>
         <span className="grid grid-rows-2 w-full gap-[1rem]">
             <button className="btn btn-success text-white">Continue</button>
-            <button className="btn btn-error text-white">Cancel</button>
+            <button className="btn btn-error text-white" onClick={()=>orderInfo.cancelOrder()}>Cancel</button>
         </span>
       </span>
     </div>
@@ -64,15 +74,15 @@ const ItemComponent=(itemInfo: ItemProps)=>{
         <span className="grid grid-rows-2 h-[100px]">
             <span className="flex justify-between">
                 <h1 className="text-lg">{itemInfo.foodName}</h1>
-                <h3>${itemInfo.foodPrice}</h3>
+                <h3>${itemInfo.quantity? (itemInfo.foodPrice * itemInfo.quantity).toFixed(2): itemInfo.foodPrice}</h3>
                 <span>
                   <span>Qty: {itemInfo.quantity}</span>
-                  <button className="btn btn-outline btn-primary">+</button>
-                  <button className="btn btn-outline btn-secondary">-</button>
+                  <button className="btn btn-outline btn-primary"   onClick={()=>itemInfo.actions.increaseQty(itemInfo.foodName)}>+</button>
+                  <button className="btn btn-outline btn-secondary" onClick={()=>itemInfo.actions.decreaseQty(itemInfo.foodName)}>-</button>
                 </span>
             </span>
             <span className="flex justify-between">
-              <button className="btn btn-outline bg-[--remove-button-bg-color] hover:bg-none hover:outline-[--remove-button-bg-color] text-white">Remove</button>
+              <button className="btn btn-outline bg-[--remove-button-bg-color] hover:bg-none hover:outline-[--remove-button-bg-color] text-white" onClick={()=>itemInfo.actions.removeItem(itemInfo.foodName)}>Remove</button>
             </span>
         </span>
     )
